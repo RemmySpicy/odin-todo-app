f5379a534b8a7eaa0c43dabfa3b37c6f0c1d02ab
O.Remilekun <remmylknolaotan@gmail.com>
2023-08-17 10:05:56 +0100

    Add sass file laouts

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,16 @@ module.exports = {
     },
     modules: {
         rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+       {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
